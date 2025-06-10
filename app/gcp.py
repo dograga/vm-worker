e34056881a260fa702ee97b32e4f0d79bcb007ce
gaurav.dogra@gmail.com <gaurav.dogra@gmail.com>
2025-06-10 16:14:09 +0800

    Test

--- app/gcp.py
+++ app/gcp.py
@@ -9,7 +9,7 @@ from google.cloud import container_v1
 from google.protobuf.timestamp_pb2 import Timestamp
 import datetime
 import os
-from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToDict
 from fastapi.responses import JSONResponse
 import json
 from app.utils.config_loader import load_config
@@ -187,7 +187,8 @@ def schedule_maintenance(req: dataclass.MaintenanceWindowRequest):
             maintenance_policy=maintenance_policy
         )
         response = client.set_maintenance_policy(request=request)
-        return JSONResponse(content=json.loads(MessageToJson(response)))
+        response_dict = MessageToDict(response, preserving_proto_field_name=True, including_default_value_fields=False)
+        return response_dict
     except Exception as e:
         logger.error(f"Error scheduling maintenance: {e}")
         raise HTTPException(status_code=500, detail=f"Error scheduling maintenance: {str(e)}")
